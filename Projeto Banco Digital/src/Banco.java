9d763b9474a8db9bb5df13ac5efe4dab40f0d566
LucasFeitosaaa <lucas_aguiarf@hotmail.com>
2024-06-16 08:06:22 -0300

    Projeto Banco Digital

---
+++ Projeto Banco Digital/src/Banco.java
@@ -0,0 +1,5 @@
+public class Banco implements ContaCorrente, ContaPoupança, Cliente  {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
