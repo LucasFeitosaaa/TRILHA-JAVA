d6b2ef653f81ae5c1c8b28a734b80f3a5f379e83
LucasFeitosaaa <lucas_aguiarf@hotmail.com>
2024-09-28 21:11:30 -0300

    Teste com o framework Springboot.

---
+++ projetospringbootdio/src/main/java/com/dio/projetospringbootdio/ProjetospringbootdioApplication.java
@@ -0,0 +1,13 @@
+package com.dio.projetospringbootdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringbootdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringbootdioApplication.class, args);
+	}
+
+}
---
+++ projetospringbootdio/src/test/java/com/dio/projetospringbootdio/ProjetospringbootdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.projetospringbootdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringbootdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
